c06ced64aa2420a8d44a8e3d944e6a975dd0d1fe
chemisky <yves.chemisky@gmail.com>
2016-03-27 20:03:23 -0400

    Ashby enclose and ellipsoid
    New shapes for the Ashby maps

--- python-setup/simmit/ashby.py
+++ python-setup/simmit/ashby.py
@@ -71,3 +71,71 @@ def poly_convexHull(points, color, coef_multi=0.1, rad=0.3, lw=2):
     edge = patches.PathPatch(path, edgecolor=color, facecolor='none', lw=lw)
     plt.gca().add_patch(patch)
     plt.gca().add_patch(edge)
+
+
+def poly_enclose(points, color, inc=1.2, rad=0.3, lw=2):
+    """
+        Plot the convex hull around a set of points as a
+        shaded polygon.
+        """
+    hull = ConvexHull(points)
+    
+    
+    cent = np.mean(points, 0)
+    pts = []
+    for pt in points[hull.vertices]:
+        pts.append(pt.tolist())
+    #        pts.append(pt.tolist())
+    
+    #    pts.sort(key=lambda p: np.arctan2(p[1] - cent[1],
+    #                                    p[0] - cent[0]))
+    #    pts = pts[0::2]  # Deleting duplicates
+    pts.insert(len(pts), pts[0])
+
+
+verts = inc*(np.array(pts)- cent) + cent
+    verts2 = np.zeros((3*verts.shape[0]-2,2))
+    verts2[0::3] = verts
+    verts2[1::3,:] = (1-rad)*verts[0:-1,:] + rad*verts[1:,:]
+    verts2[2::3,:] = rad*verts[0:-1,:] + (1-rad)*verts[1:,:]
+    verts2[0:-1] = verts2[1:]
+    verts2[-1] = verts2[0]
+    
+    codes = [Path.MOVETO, Path.LINETO, Path.CURVE3,]
+    for j in range(len(pts)-2):
+        codes.extend([Path.CURVE3, Path.LINETO, Path.CURVE3,])
+    codes.append(Path.CURVE3)
+
+
+    path = Path(verts2, codes)
+    patch = patches.PathPatch(path, facecolor=color, lw=0, alpha=0.2)
+    edge = patches.PathPatch(path, edgecolor=color, facecolor='none', lw=lw)
+    plt.gca().add_patch(patch)
+    plt.gca().add_patch(edge)
+
+def ellip_enclose(points, color, inc=1.2, lw=2, nst=2):
+    """
+        Plot the minimum ellipse around a set of points.
+        
+        Based on:
+        https://github.com/joferkington/oost_paper_code/blob/master/error_ellipse.py
+        """
+    
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+    
+    x = points[:,0]
+    y = points[:,1]
+    cov = np.cov(x, y)
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+    w, h = 2 * nst * np.sqrt(vals)
+    center = np.mean(points, 0)
+    ell = patches.Ellipse(center, width=inc*w, height=inc*h, angle=theta,
+                          facecolor=color, alpha=0.2, lw=0)
+    edge = patches.Ellipse(center, width=inc*w, height=inc*h, angle=theta,
+                                                 facecolor='none', edgecolor=color, lw=lw)
+    plt.gca().add_artist(ell)
+    plt.gca().add_artist(edge)

06d32fa70c6d3707859a0e5e933ec54cb5a65336
chemisky <yves.chemisky@gmail.com>
2016-05-08 08:45:00 -0400

    modified solver.cpp for angles

--- smartplus-builder/src/smartplus_wrappers/Libraries/Solver/solver.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Solver/solver.cpp
@@ -17,13 +17,13 @@ using namespace arma2numpy;
 namespace smartpy {
 
 //This function computes the response of materials for an homogeneous mixed thermomechanical loading path    
-void solver(const bp::str &umat_name_py, const bn::ndarray &props, const int &nstatev, const double &rho, const double &c_p, const bp::str &pathfile_py, const bp::str &outputfile_py) {
+void solver(const bp::str &umat_name_py, const bn::ndarray &props, const int &nstatev, const double &psi_rve, const double &theta_rve, const double &phi_rve, const double &rho, const double &c_p, const bp::str &pathfile_py, const bp::str &outputfile_py) {
     vec v = array2vec(props);
 
     std::string umat_name = bp::extract<std::string>(umat_name_py);
     std::string pathfile = bp::extract<std::string>(pathfile_py);
     std::string outputfile = bp::extract<std::string>(outputfile_py);
-    smart::solver(umat_name, v, nstatev, rho, c_p, pathfile, outputfile);
+    smart::solver(umat_name, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p, pathfile, outputfile);
 }
 
 } //namepsace smartpy

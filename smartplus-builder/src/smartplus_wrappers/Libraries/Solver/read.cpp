2a1eaeb6d6e6d59b956f8b57c9e58141258ed9f2
chemisky <yves.chemisky@gmail.com>
2016-05-08 08:44:52 -0400

    modified read.cpp for angles

--- smartplus-builder/src/smartplus_wrappers/Libraries/Solver/read.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Solver/read.cpp
@@ -15,10 +15,10 @@ using namespace arma2numpy;
 namespace smartpy {
 
 //This function reads material properties to prepare a simulation
-void read_matprops(int &nprops, bn::ndarray &props, int &nstatev, double &rho, double &c_p) {
+void read_matprops(int &nprops, bn::ndarray &props, int &nstatev, double &psi_rve, double &theta_rve, double &phi_rve, double &rho, double &c_p) {
     vec v = array2vec(props);
     string umat_name;
-    smart::read_matprops(umat_name, nprops, v, nstatev, rho, c_p);
+    smart::read_matprops(umat_name, nprops, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p);
     props = vec2array(v);
 }
         

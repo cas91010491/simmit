1ff3fa878b207336f07957f1e74ba39d8d849c61
chemisky <yves.chemisky@gmail.com>
2016-06-03 08:25:49 -0400

    Exposed identification constants

---
+++ smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Identification/identification.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <boost/python.hpp>
+#include <boost/numpy.hpp>
+
+namespace smartpy{
+
+//This function computes the response of materials for an homogeneous mixed thermomechanical loading path
+void identification_solver(const int &, const int &, const int &, const int &, const int &, const int &, const int &, const int &, const int &, const boost::python::str &, const boost::python::str &, const boost::python::str &);
+
+boost::python::list read_constants_py(const int &, const int &);
+    
+void copy_constants_py(const boost::python::list &, const std::string &, const std::string &);
+    
+void apply_constants_py(const boost::python::list &, const std::string &);
+
+boost::python::list read_parameters_py(const int &);
+
+void copy_parameters_py(const boost::python::list &, const std::string &, const std::string &);
+
+void apply_parameters_py(const boost::python::list &, const std::string &);
+
+double calc_cost(const int &, const std::string &);
+    
+} //namespace smartpy

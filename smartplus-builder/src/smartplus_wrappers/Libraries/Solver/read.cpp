a7099a276e1aef83509f5b220c612f1bd8696724
chemisky <yves.chemisky@gmail.com>
2016-06-23 11:08:12 -0400

    Added folder and materials data file name

--- smartplus-builder/src/smartplus_wrappers/Libraries/Solver/read.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Solver/read.cpp
@@ -7,6 +7,7 @@
 #include <smartplus/Libraries/Solver/read.hpp>
 #include <simmit/smartplus_wrappers/Libraries/Solver/read.hpp>
 
+namespace bp = boost::python;
 namespace bn = boost::numpy;
 using namespace std;
 using namespace arma;
@@ -15,10 +16,12 @@ using namespace arma2numpy;
 namespace smartpy {
 
 //This function reads material properties to prepare a simulation
-void read_matprops(int &nprops, bn::ndarray &props, int &nstatev, double &psi_rve, double &theta_rve, double &phi_rve, double &rho, double &c_p) {
+void read_matprops(int &nprops, bn::ndarray &props, int &nstatev, double &psi_rve, double &theta_rve, double &phi_rve, double &rho, double &c_p, const bp::str &path_data_py, const bp::str &materialfile_py) {
     vec v = array2vec(props);
     string umat_name;
-    smart::read_matprops(umat_name, nprops, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p);
+    string path_data = bp::extract<std::string>(path_data_py);
+    string materialfile = bp::extract<std::string>(materialfile_py);
+    smart::read_matprops(umat_name, nprops, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p, path_data, materialfile);
     props = vec2array(v);
 }
         

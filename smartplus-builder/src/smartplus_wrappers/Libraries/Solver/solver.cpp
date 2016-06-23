d8b4f36748a0db36b9d3a88b8d46bb4e1196d79e
chemisky <yves.chemisky@gmail.com>
2016-06-23 11:08:17 -0400

    Added folder and materials data file name

--- smartplus-builder/src/smartplus_wrappers/Libraries/Solver/solver.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Solver/solver.cpp
@@ -17,13 +17,15 @@ using namespace arma2numpy;
 namespace smartpy {
 
 //This function computes the response of materials for an homogeneous mixed thermomechanical loading path    
-void solver(const bp::str &umat_name_py, const bn::ndarray &props, const int &nstatev, const double &psi_rve, const double &theta_rve, const double &phi_rve, const double &rho, const double &c_p, const bp::str &pathfile_py, const bp::str &outputfile_py) {
+void solver(const bp::str &umat_name_py, const bn::ndarray &props, const int &nstatev, const double &psi_rve, const double &theta_rve, const double &phi_rve, const double &rho, const double &c_p, const bp::str &path_data_py, const bp::str &path_results_py, const bp::str &pathfile_py, const bp::str &outputfile_py) {
     vec v = array2vec(props);
 
     std::string umat_name = bp::extract<std::string>(umat_name_py);
+    std::string path_data = bp::extract<std::string>(path_data_py);
+    std::string path_results = bp::extract<std::string>(path_results_py);
     std::string pathfile = bp::extract<std::string>(pathfile_py);
     std::string outputfile = bp::extract<std::string>(outputfile_py);
-    smart::solver(umat_name, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p, pathfile, outputfile);
+    smart::solver(umat_name, v, nstatev, psi_rve, theta_rve, phi_rve, rho, c_p, path_data, path_results, pathfile, outputfile);
 }
 
 } //namepsace smartpy

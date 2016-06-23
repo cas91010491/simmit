35b7fe8524215071531ae8977b882baa1acd18a1
chemisky <yves.chemisky@gmail.com>
2016-06-23 11:07:39 -0400

    Added folder and materials data file name

--- smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Solver/solver.hpp
+++ smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Solver/solver.hpp
@@ -5,7 +5,7 @@
 namespace smartpy{
 
 //This function computes the response of materials for an homogeneous mixed thermomechanical loading path
-void solver(const boost::python::str &, const boost::numpy::ndarray &, const int &, const double &, const double &, const double &, const double &, const double &, const boost::python::str &, const boost::python::str &);
+void solver(const boost::python::str &, const boost::numpy::ndarray &, const int &, const double &, const double &, const double &, const double &, const double &, const boost::python::str &, const boost::python::str &, const boost::python::str &, const boost::python::str &);
  
 //void solver();
     

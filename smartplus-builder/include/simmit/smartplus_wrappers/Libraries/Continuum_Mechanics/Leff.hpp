a8fb2c01f122fa906c3200a24d8aeef75751229b
chemisky <yves.chemisky@gmail.com>
2016-08-21 17:28:45 -0400

    removed c_p, theta

--- smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Continuum_Mechanics/Leff.hpp
+++ smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Continuum_Mechanics/Leff.hpp
@@ -5,6 +5,6 @@
 namespace smartpy{
 
 //return the elastic stiffness tensor of a composite material
-    boost::numpy::ndarray L_eff(const boost::python::str &, const boost::numpy::ndarray &, const int &, const double &, const double &, const double &, const double &, const double &, const boost::python::str &);
+    boost::numpy::ndarray L_eff(const boost::python::str &, const boost::numpy::ndarray &, const int &, const double &, const double &, const double &, const boost::python::str &);
     
 } //namespace smartpy
--- smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Solver/read.hpp
+++ smartplus-builder/include/simmit/smartplus_wrappers/Libraries/Solver/read.hpp
@@ -5,6 +5,6 @@
 namespace smartpy{
 
 //This function reads material properties to prepare a simulation
-    void read_matprops(int &, boost::numpy::ndarray &, int &, double &, double &, double &, double &, double &, const boost::python::str &, const boost::python::str &);
+    void read_matprops(int &, boost::numpy::ndarray &, int &, double &, double &, double &, const boost::python::str &, const boost::python::str &);
     
 } //namespace smartpy

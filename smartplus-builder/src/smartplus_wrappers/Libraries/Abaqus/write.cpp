f0f74e9fe6d089ed186ea4393ac0513d527b7efd
chemisky <yves.chemisky@gmail.com>
2017-02-06 16:45:54 +0100

    Added write materials

---
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Abaqus/write.cpp
@@ -0,0 +1,28 @@
+
+#include <armadillo>
+#include <boost/python.hpp>
+#include <boost/numpy.hpp>
+#include <simmit/arma2numpy/numpy_arma.hpp>
+
+#include <smartplus/Libraries/Solver/read.hpp>
+#include <simmit/smartplus_wrappers/Libraries/Solver/read.hpp>
+
+namespace bp = boost::python;
+namespace bn = boost::numpy;
+using namespace std;
+using namespace arma;
+using namespace arma2numpy;
+
+namespace smartpy {
+
+//This function reads material properties to prepare a simulation
+void write_materials(int &nprops, bn::ndarray &props, int &nstatev, double &psi_rve, double &theta_rve, double &phi_rve, const bp::str &path_data_py, const bp::str &materialfile_py) {
+    vec v = array2vec(props);
+    string umat_name;
+    string path_data = bp::extract<std::string>(path_data_py);
+    string materialfile = bp::extract<std::string>(materialfile_py);
+    smart::read_matprops(umat_name, nprops, v, nstatev, psi_rve, theta_rve, phi_rve, path_data, materialfile);
+    props = vec2array(v);
+}
+        
+} //namepsace smartpy

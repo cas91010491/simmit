311eb7195fa46cda165dc07ba3753f7ed03f6023
chemisky <yves.chemisky@gmail.com>
2017-08-11 11:04:26 +0200

    Changes to Boost 1.63 inclusion of boost::numpy

--- smartplus-builder/src/smartplus_wrappers/Libraries/Identification/constants.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Identification/constants.cpp
@@ -1,14 +1,15 @@
-
-
 ///@file constants.cpp
 ///@brief Handle of input constants exposed in python
 ///@version 0.9
+
 
 #include <iostream>
 #include <fstream>
 #include <assert.h>
 #include <math.h>
 #include <armadillo>
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
 
 #include <simmit/arma2numpy/list_vector.hpp>
 #include <simmit/arma2numpy/numpy_arma.hpp>
@@ -17,44 +18,47 @@
 
 using namespace std;
 using namespace arma;
+using namespace arma2numpy;
+namespace bp = boost::python;
+namespace bn = boost::python::numpy;
 
 namespace smartpy{
     
 //-------------------------------------------------------------
-smart::constants build_constants_full(const int &mnumber, const double &mvalue, const boost::numpy::ndarray &minput_values, const std::string &mkey, const int &mninput_files, const boost::python::list &minput_files)
+smart::constants build_constants_full(const int &mnumber, const double &mvalue, const bn::ndarray &minput_values, const std::string &mkey, const int &mninput_files, const bp::list &minput_files)
 //-------------------------------------------------------------
 {
     smart::constants a;
     a.number = mnumber;
     a.value = mvalue;
-    a.input_values = arma2numpy::array2vec(minput_values);
+    a.input_values = array2vec(minput_values);
     a.key = mkey;
     a.ninput_files = mninput_files;
-    a.input_files = arma2numpy::py_list_to_std_vector_string(minput_files);
+    a.input_files = py_list_to_std_vector_string(minput_files);
     return a;
 }
 
 //------------------------------------------------------
-boost::numpy::ndarray constants_get_input_values(smart::constants &self) {
-    return arma2numpy::vec2array(self.input_values);
+bn::ndarray constants_get_input_values(smart::constants &self) {
+    return vec2array(self.input_values);
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-boost::python::list constants_get_input_files(smart::constants &self) {
-    return arma2numpy::std_vector_to_py_list_string(self.input_files);
+bp::list constants_get_input_files(smart::constants &self) {
+    return std_vector_to_py_list_string(self.input_files);
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-void constants_set_input_values(smart::constants &self, const boost::numpy::ndarray &minput_values) {
-    self.input_values = arma2numpy::array2vec(minput_values);
+void constants_set_input_values(smart::constants &self, const bn::ndarray &minput_values) {
+    self.input_values = array2vec(minput_values);
 }
 //------------------------------------------------------
 
 //------------------------------------------------------
-void constants_set_input_files(smart::constants &self, const boost::python::list &minput_values) {
-    self.input_files = arma2numpy::py_list_to_std_vector_string(minput_values);
+void constants_set_input_files(smart::constants &self, const bp::list &minput_values) {
+    self.input_files = py_list_to_std_vector_string(minput_values);
     self.ninput_files = self.input_files.size();
 }
 //------------------------------------------------------

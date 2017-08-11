5e93f515234a787b1efc5a5149e32b4ba54eb72d
chemisky <yves.chemisky@gmail.com>
2017-08-11 11:04:39 +0200

    Changes to Boost 1.63 inclusion of boost::numpy

--- smartplus-builder/src/smartplus_wrappers/Libraries/Identification/parameters.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Identification/parameters.cpp
@@ -9,19 +9,25 @@
 #include <assert.h>
 #include <math.h>
 #include <armadillo>
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+
 
 #include <simmit/arma2numpy/list_vector.hpp>
 #include <simmit/arma2numpy/numpy_arma.hpp>
 #include <smartplus/Libraries/Identification/parameters.hpp>
 #include <simmit/smartplus_wrappers/Libraries/Identification/parameters.hpp>
 
+namespace bp = boost::python;
+namespace bn = boost::python::numpy;
 using namespace std;
 using namespace arma;
+using namespace arma2numpy;
 
 namespace smartpy{
     
 //-------------------------------------------------------------
-smart::parameters build_parameters_full(const int &mnumber, const double &mmin_value, const double &mmax_value, const std::string &mkey, const int &mninput_files, const boost::python::list &minput_files)
+smart::parameters build_parameters_full(const int &mnumber, const double &mmin_value, const double &mmax_value, const std::string &mkey, const int &mninput_files, const bp::list &minput_files)
 //-------------------------------------------------------------
 {
     smart::parameters a;
@@ -31,7 +37,7 @@ smart::parameters build_parameters_full(const int &mnumber, const double &mmin_v
     a.value = (a.min_value+a.max_value)/2.;
     a.key = mkey;
     a.ninput_files = mninput_files;
-    a.input_files = arma2numpy::py_list_to_std_vector_string(minput_files);
+    a.input_files = py_list_to_std_vector_string(minput_files);
     return a;
 }
 
@@ -42,8 +48,8 @@ boost::python::list parameters_get_input_files(smart::parameters &self) {
 //------------------------------------------------------
 
 //------------------------------------------------------
-void parameters_set_input_files(smart::parameters &self, const boost::python::list &minput_values) {
-    self.input_files = arma2numpy::py_list_to_std_vector_string(minput_values);
+void parameters_set_input_files(smart::parameters &self, const bp::list &minput_values) {
+    self.input_files = py_list_to_std_vector_string(minput_values);
     self.ninput_files = self.input_files.size();
 }
 //------------------------------------------------------

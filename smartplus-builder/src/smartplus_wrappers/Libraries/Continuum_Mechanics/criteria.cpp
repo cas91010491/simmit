e3ff3f3734c90488ab1b179b1e4639e883d770fe
chemisky <yves.chemisky@gmail.com>
2017-08-11 10:57:10 +0200

    Changes to Boost 1.63 inclusion of boost::numpy

--- smartplus-builder/src/smartplus_wrappers/Libraries/Continuum_Mechanics/criteria.cpp
+++ smartplus-builder/src/smartplus_wrappers/Libraries/Continuum_Mechanics/criteria.cpp
@@ -2,13 +2,13 @@
 #include <armadillo>
 #include <string>
 #include <boost/python.hpp>
-#include <boost/numpy.hpp>
+#include <boost/python/numpy.hpp>
 #include <simmit/arma2numpy/numpy_arma.hpp>
 
 #include <smartplus/Libraries/Continuum_Mechanics/criteria.hpp>
 #include <simmit/smartplus_wrappers/Libraries/Continuum_Mechanics/criteria.hpp>
 
-namespace bn = boost::numpy;
+namespace bn = boost::python::numpy;
 using namespace std;
 using namespace arma;
 using namespace arma2numpy;
@@ -16,38 +16,38 @@ using namespace arma2numpy;
 namespace smartpy {
 
 //This function returns the Prager equivalent stress.
-double Prager_stress(const boost::numpy::ndarray &nd, const double &b, const double &n) {
+double Prager_stress(const bn::ndarray &nd, const double &b, const double &n) {
     vec v = array2vec(nd);
     return smart::Prager_stress(v,b,n);
 }
 
 //This function returns the derivative of the Prager equivalent stress.
-boost::numpy::ndarray dPrager_stress(const boost::numpy::ndarray &nd, const double &b, const double &n) {
+bn::ndarray dPrager_stress(const bn::ndarray &nd, const double &b, const double &n) {
     vec v = array2vec(nd);
     return vec2array(smart::dPrager_stress(v,b,n));
 }
 
 //This function returns the Tresca equivalent stress.
-double Tresca_stress(const boost::numpy::ndarray &nd) {
+double Tresca_stress(const bn::ndarray &nd) {
     vec v = array2vec(nd);
     return smart::Tresca_stress(v);
 }
 
 //This function returns the derivative of the Tresca equivalent stress.
-boost::numpy::ndarray dTresca_stress(const boost::numpy::ndarray &nd) {
+bn::ndarray dTresca_stress(const bn::ndarray &nd) {
     vec v = array2vec(nd);
     return vec2array(smart::dTresca_stress(v));
 }
     
 //This function computes the selected equivalent stress function
-double Eq_stress(const boost::numpy::ndarray &nd, const string &eq_type, const boost::numpy::ndarray &ndparam) {
+double Eq_stress(const bn::ndarray &nd, const string &eq_type, const bn::ndarray &ndparam) {
     vec v = array2vec(nd);
     vec param = array2vec(ndparam);
     return smart::Eq_stress(v,eq_type,param);
 }
 
 //This function computes the deriavtive of the selected equivalent stress function
-boost::numpy::ndarray dEq_stress(const boost::numpy::ndarray &nd, const string &eq_type, const boost::numpy::ndarray &ndparam) {
+bn::ndarray dEq_stress(const bn::ndarray &nd, const string &eq_type, const bn::ndarray &ndparam) {
     vec v = array2vec(nd);
     vec param = array2vec(ndparam);
     return vec2array(smart::dEq_stress(v,eq_type,param));
